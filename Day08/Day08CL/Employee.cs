23e63aaeaa702c15a8e984bd7d804950baf667ce
Garrett Girod <girodg@hotmail.com>
2023-03-15 09:38:45 -0400

    inheritance

---
+++ Day08/Day08CL/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day08CL
+{
+    //only 1 base class, implement N number of interfaces
+    public class Employee : Person
+    {
+        public double Salary { get; set; }
+
+        public Employee(double salary, string name, int age) : base(name, age) //new Person(name, age)
+        {
+            Salary = salary;
+        }
+    }
+}
---
+++ Day08/Day08CL/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day08CL
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void Eat(string food)
+        {
+            Console.WriteLine($"I'm hungry. let's eat some {food}.");
+        }
+    }
+}
